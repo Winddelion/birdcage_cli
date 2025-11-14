2c648486249c3eb694a3d97ac867bd8d501aa014
winddelion <225583612+Winddelion@users.noreply.github.com>
2025-11-14 15:37:24 +0300

    added placeholder for options parsing

--- src/main.rs
+++ src/main.rs
@@ -11,10 +11,44 @@ fn print_help() {
     println!("-d -> delete password");
     println!("-rh -> rename the handler");
     println!("-rp -> change password");
-    println!("-v --change-vault -> change default vault");
+}
+
+fn request_password() {
+    println!("please enter current vault's master pasword: ")
+}
+
+fn lorem_ipsum() {
+    println!("Something")
 }
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     let mut _verbose = false;
+
+    let mut arg_check = 1;
+    while args.len() > 1 {
+        match args[1].as_str() {
+            "-h" | "--help" => {
+                print_help();
+            }
+            "-l" | "--list" => {
+                lorem_ipsum();
+            }
+            "-a" | "--add" => {
+                lorem_ipsum();
+            }
+            "-d" | "--delete" => {
+                lorem_ipsum();
+            }
+            "-rh" => {
+                lorem_ipsum();
+            }
+            "-rp" => {
+                lorem_ipsum();
+            }
+            _ => {
+                eprintln!("Error. Failed to parse arguments");
+            }
+        }
+    }
 }
